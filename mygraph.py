88896a02058232ac413a04d7a753dde1c5d3264f
Christian Andsager <Christian_Andsager1@Baylor.edu>
2023-08-23 15:37:30 -0500

    initial commit

---
+++ mygraph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+#Create VE
+#Activate your VE
+#Install your 3rd party library in VE
+#Make sure enviroment is active in the terminal and bottom
